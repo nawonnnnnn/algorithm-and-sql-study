0df60046c4a345830ea994233fb3b03e8d4fab40
LeeNawon <81615776+nawonnnnnn@users.noreply.github.com>
2026-04-15 23:46:57 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.43 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        while (s < e) {
+            char temp = sb.charAt(s);
+            sb.setCharAt(s, sb.charAt(e));
+            sb.setCharAt(e, temp);
+             
+            s++;
+            e--;
+        }
+        
+        return sb.toString();
+    }
+}
